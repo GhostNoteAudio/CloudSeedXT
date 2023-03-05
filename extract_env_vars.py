73388351dec6de6a4e773de7542aa19e95705a77
GhostNoteAudio <valdiorn+ghostnote@gmail.com>
2023-03-05 01:20:06 +0000

    fix env var stuff

--- extract_env_vars.py
+++ extract_env_vars.py
@@ -1,6 +1,7 @@
 import xml.etree.ElementTree as ET
 tree = ET.parse('CloudSeedXT.jucer')
 root = tree.getroot()
+user_note_data = root.attrib['userNotes'].replace('\\', '\\\\')
 
 with open('env.txt', 'w') as f:
     f.write(f"VERSION={root.attrib['version']}\n")
@@ -9,7 +10,7 @@ with open('env.txt', 'w') as f:
     f.write(f"COMPANY_NAME={root.attrib['companyName']}\n")
     f.write(f"COMPANY_WEBSITE={root.attrib['companyWebsite']}\n")
     f.write(f"VS_BUILD_DIR=./{root.find('EXPORTFORMATS').find('VS2022').attrib['targetFolder']}\n")
-    f.write(f"{root.attrib['userNotes'].replace('\\', '\\\\')}\n")
+    f.write(f"{user_note_data}\n")
 
 
 with open('env.txt', 'r') as f:
